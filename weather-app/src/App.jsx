2c4cac16024eb23b9e38b85b03d61569499c1c73
Abhishek Katkar <katkarabhi91@gmail.com>
2022-05-28 10:35:44 +0530

    d

--- weather-app/src/App.jsx
+++ weather-app/src/App.jsx
@@ -1,10 +1,25 @@
 import logo from './logo.svg';
 import './App.css';
+import { Navbar } from './Components/Navbar';
 
 function App() {
   return (
-    <div className="App">
-      <h1>Hello world!!</h1>
+    <div className="App">    
+      <Navbar/>
+
+
+      
+      {/* <div>
+      <iframe
+  width="600"
+  height="450"
+  // style="border:0"
+  // loading="lazy"
+  
+  // referrerpolicy="no-referrer-when-downgrade"
+  src="https://www.google.com/maps/embed/v1/place?key=AIzaSyAWelLTPW340QBxVkEDipz0q9cODb0T8GI&q=satara">
+</iframe>
+      </div> */}
     </div>
   );
 }
---
+++ weather-app/src/Components/Navbar.jsx
@@ -0,0 +1,16 @@
+import "../Components/Navbar.css";
+
+
+export const Navbar = () => {
+
+
+    return(
+        <div className="nav">
+            <div>
+            <input className="search" type="text" />
+            <button className="btn">Search</button>
+            </div>
+            
+        </div>
+    )
+}
