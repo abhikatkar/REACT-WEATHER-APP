c926e172c3cc21d5acb98101c3bae4e103800841
Abhishek Katkar <katkarabhi91@gmail.com>
2022-05-31 00:36:24 +0530

    d

--- weather-app/src/Components/Main.jsx
+++ weather-app/src/Components/Main.jsx
@@ -8,26 +8,28 @@ export const Main = ({value})=>{
 
     const [data1, setData1] = useState([]);
 
+    const [data2, setData2] = useState([]);
+
     const z = `https://www.google.com/maps/embed/v1/place?key=AIzaSyAWelLTPW340QBxVkEDipz0q9cODb0T8GI&q=${value}`;
 
 useEffect(()=>{
-    axios.get("https://api.openweathermap.org/data/2.5/weather?q=satara&appid=06e57ae98533ae7240b4a10cfa49e0ee&units=metric")
+    axios.get("https://api.openweathermap.org/data/2.5/weather?q=pune&appid=06e57ae98533ae7240b4a10cfa49e0ee&units=metric")
     .then((res) => {setData1(res.data)
         console.log(res.data);
+        
     });
     
 },[]);
 
 useEffect(()=>{
     axios.get(`https://api.openweathermap.org/data/2.5/weather?q=${value}&appid=06e57ae98533ae7240b4a10cfa49e0ee&units=metric`)
-    .then((res) => {setData1(res.data)
-        console.log(res.data);
+    .then((res) => {setData2(res.data)
+        // console.log(res.data);
     });
     
 },[value]);
 
    
- 
     
     
 
@@ -38,7 +40,7 @@ useEffect(()=>{
                <div className="box-1">
                    <h3>{data1.name}</h3>
                    <h1>23<sup>o</sup>C</h1>
-                   <h3>clouds</h3>
+                   <h3></h3>
                </div>
                <div className="box-2">
                    <p>Felt Temp. - 23.85<sup>o</sup>C</p>
