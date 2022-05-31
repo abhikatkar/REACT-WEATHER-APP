ea608591249824d38b9f184c5dce877f41f060d1
Abhishek Katkar <katkarabhi91@gmail.com>
2022-05-31 09:02:54 +0530

    done

--- weather-app/src/Components/Main.jsx
+++ weather-app/src/Components/Main.jsx
@@ -7,13 +7,11 @@ import { useEffect, useState } from "react";
 export const Main = ({value})=>{
 
 
-    const [log,setLog] = useState();
-
-      const [lat,setLat] = useState();
-
       const [data2 , setData2] = useState({
-        daily:[{dt:""}],
-
+       
+        daily:[{temp:{day:""}},{temp:{day:""}},{temp:{day:""}},{temp:{day:""}},{temp:{day:""}}],
+        daily:[{temp:{night:""}},{temp:{night:""}},{temp:{night:""}},{temp:{night:""}},{temp:{night:""}}],
+        
       });
 
     const [data1, setData1] = useState({
@@ -29,22 +27,14 @@ export const Main = ({value})=>{
     });
 
    
-  let z = `https://www.google.com/maps/embed/v1/place?key=AIzaSyAWelLTPW340QBxVkEDipz0q9cODb0T8GI&q=${value!=""? value:"pune"}`;
+  let z = `https://www.google.com/maps/embed/v1/place?key=AIzaSyAWelLTPW340QBxVkEDipz0q9cODb0T8GI&q=${value!=""? value:"Delhi"}`;
 
 
     useEffect(()=>{
         axios.get(`https://api.openweathermap.org/data/2.5/weather?q=${value}&appid=06e57ae98533ae7240b4a10cfa49e0ee&units=metric`)
-        .then((res) => {setData1({...res.data})
-            // console.log(res.data.coord.lon,res.data.coord.lat);  
-            // setLog(res.data.coord.lon)
-            // setLat(res.data.coord.lat) 
-            axios.get(`https://api.openweathermap.org/data/2.5/onecall?lat=${res.data.coord.lat}&lon=${res.data.coord.lon}&exclude=current,minutely,hourly,alerts&appid=06e57ae98533ae7240b4a10cfa49e0ee&units=metric`)
-        .then((rese) => {setData2({...rese.data})
-                 
-        });       
+        .then((res) => {setData1({...res.data})            
+            cityWeekInfo(res.data.coord.lat,res.data.coord.lon)
         });
-
-        
         
     },[value]);
 
@@ -52,25 +42,30 @@ export const Main = ({value})=>{
 
 
 useEffect(()=>{
-    axios.get("https://api.openweathermap.org/data/2.5/weather?q=pune&appid=06e57ae98533ae7240b4a10cfa49e0ee&units=metric")
+    axios.get("https://api.openweathermap.org/data/2.5/weather?q=Delhi&appid=06e57ae98533ae7240b4a10cfa49e0ee&units=metric")
     .then((res) => {setData1({...res.data})
-        // console.log(res.data.coord.lon,res.data.coord.lat);
-        // setLog(res.data.coord.lon)
-            // setLat(res.data.coord.lat)
-            axios.get(`https://api.openweathermap.org/data/2.5/onecall?${res.data.coord.lat}&lon=${res.data.coord.lon}&exclude=current,minutely,hourly,alerts&appid=06e57ae98533ae7240b4a10cfa49e0ee&units=metric`)
-        .then((rese) => {setData2({...rese.data})
-                 
-        }); 
-    });
-
-
+        cityWeekInfo(res.data.coord.lat,res.data.coord.lon)
+    }); 
     
     
-},[]);
+},[]);  
 
 
+async function cityWeekInfo(latitude, longitude) {
+    
+    let url = `https://api.openweathermap.org/data/2.5/onecall?lat=${latitude}&lon=${longitude}&exclude=current,minutely,hourly,alerts&appid=b3dfaeaf2defd23abad8ac8117387f27&units=metric`
+    try {
+        let responce = await fetch(url);
+        let data = await responce.json();
+       
+        
+        setData2(data);
+    }
+    catch (err) {
+        console.log(err)
+    }
 
-   
+}
     
 
 
@@ -98,39 +93,34 @@ useEffect(()=>{
            </div>
            <div className="second-container">
                <div className="small-box">
-                   <p>{data2.daily[0].dt!=""&&data2.daily[0].dt}</p>
-                   <p>Sunday</p>
-                   <p><BsFillSunFill /> 30<sup>o</sup>C</p>
-                   <p><MdNightsStay /> 23<sup>o</sup>C</p>
-                   <p>clear</p>
+                   
+                   <p><BsFillSunFill /> {data2.daily[0].temp.day!=""&&data2.daily[0].temp.day}<sup>o</sup>C</p>
+                   <p><MdNightsStay /> {data2.daily[0].temp.night!=""&&data2.daily[0].temp.night}<sup>o</sup>C</p>
+                  
                </div>
                <div className="small-box">
-                   {/* <p>{data2.daily[1].dt!=""&&data2.daily[1].dt}</p> */}
-                   <p>Sunday</p>
-                   <p><BsFillSunFill /> 30<sup>o</sup>C</p>
-                   <p><MdNightsStay /> 23<sup>o</sup>C</p>
-                   <p>clear</p>
+                  
+                   <p><BsFillSunFill />{data2.daily[1].temp.day!=""&&data2.daily[1].temp.day}<sup>o</sup>C</p>
+                   <p><MdNightsStay /> {data2.daily[1].temp.night!=""&&data2.daily[1].temp.night}<sup>o</sup>C</p>
+                   
                </div>
                <div className="small-box">
-                   {/* <p>{data2.daily[2].dt!=""&&data2.daily[2].dt}</p> */}
-                   <p>Sunday</p>
-                   <p><BsFillSunFill /> 30<sup>o</sup>C</p>
-                   <p><MdNightsStay /> 23<sup>o</sup>C</p>
-                   <p>clear</p>
+                   
+                   <p><BsFillSunFill /> {data2.daily[2].temp.day!=""&&data2.daily[2].temp.day}<sup>o</sup>C</p>
+                   <p><MdNightsStay /> {data2.daily[2].temp.night!=""&&data2.daily[2].temp.night}<sup>o</sup>C</p>
+                 
                </div>
                <div className="small-box">
-                   {/* <p>{data2.daily[3].dt!=""&&data2.daily[3].dt}</p> */}
-                   <p>Sunday</p>
-                   <p><BsFillSunFill /> 30<sup>o</sup>C</p>
-                   <p><MdNightsStay /> 23<sup>o</sup>C</p>
-                   <p>clear</p>
+                 
+                   <p><BsFillSunFill /> {data2.daily[3].temp.day!=""&&data2.daily[3].temp.day}<sup>o</sup>C</p>
+                   <p><MdNightsStay /> {data2.daily[3].temp.night!=""&&data2.daily[3].temp.night}<sup>o</sup>C</p>
+                
                </div>
                <div className="small-box">
-                   {/* <p>{data2.daily[4].dt!=""&&data2.daily[4].dt}</p> */}
-                   <p>Sunday</p>
-                   <p><BsFillSunFill /> 30<sup>o</sup>C</p>
-                   <p><MdNightsStay /> 23<sup>o</sup>C</p>
-                   <p>clear</p>
+                  
+                   <p><BsFillSunFill /> {data2.daily[4].temp.day!=""&&data2.daily[4].temp.day}<sup>o</sup>C</p>
+                   <p><MdNightsStay /> {data2.daily[4].temp.night!=""&&data2.daily[4].temp.night}<sup>o</sup>C</p>
+                
                </div>
            </div>
         </div>
--- weather-app/src/Components/Navbar.jsx
+++ weather-app/src/Components/Navbar.jsx
@@ -6,12 +6,27 @@ export const Navbar = ({getItemParent}) => {
 
     const [item , setItem] = useState("");
 
+    function curr(){
+        if (navigator.geolocation) {
+            navigator.geolocation.getCurrentPosition(showPosition);
+          }
+    }
+
+    function showPosition(position) {
+
+  let loca = `https://maps.googleapis.com/maps/api/geocode/json?latlng=${position.coords.latitude},${position.coords.longitude}&sensor=true&key=AIzaSyA0zKLtY0TCm_NpVoAhcRtHEFeTsuMDLCY`;
+     
+  console.log(loca)
+
+}
+
 
     return(
         <div className="nav">
             <div>
             <input onChange={(e)=>(setItem(e.target.value))} className="search" type="text" />
             <button onClick={()=>{getItemParent(item)}}  className="btn">Search</button>
+            <button className="btn1" onClick={curr}>Current Location</button>
             </div>
             
         </div>
